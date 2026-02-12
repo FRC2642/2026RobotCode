242536fc39c16fa85ece3ccc380de2aed8392289
tutenry <rtuten10@gmail.com>
2026-02-11 20:50:43 -0500

    defined motors and encoders for intaketilt

--- src/main/java/frc/robot/subsystems/intakeTilt.java
+++ src/main/java/frc/robot/subsystems/intakeTilt.java
@@ -3,13 +3,38 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.Encoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class intakeTilt extends SubsystemBase {
+  public double maxRotateSpeed = 1;
+  public RotationPositions motorState;
+  //defining both motors on the thing
+  public TalonFX tiltA = new TalonFX(0);
+  public TalonFX tiltB = new TalonFX(1);
+  public DutyCycleEncoder encoder = new DutyCycleEncoder(0);
+  public PIDController PID = new PIDController(1,0,0);
+
+  public Trigger positionReached = new Trigger(() -> Math.abs(getEncoderValue() - motorState.position) < 0.01);
+  public double getEncoderValue(){
+    return encoder.get();
+  }
   /** Creates a new intakeTilt. */
-  public intakeTilt() {}
+  public intakeTilt() {
+    tiltA.setNeutralMode(NeutralModeValue.Brake);
+    tiltB.setNeutralMode(NeutralModeValue.Brake);
+    setDefaultCommand(runOnce(()->{
+      tiltA.set(0);
+      tiltB.set(0);
+    }));
+
+  }
 
   @Override
   public void periodic() {
