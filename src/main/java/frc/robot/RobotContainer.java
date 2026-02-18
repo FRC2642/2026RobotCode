784ffb40f1bb277b4b8057d99af97ab635e32cc6
angelere-creator <angelere@students.pittschools.org>
2026-02-17 20:51:42 -0500

    Deleted Unnecessary Variables.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,16 +14,21 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Vision;
+import frc.robot.subsystems.Climby;
 
+@SuppressWarnings("unused")
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
+    public Climby Climby = new Climby();
+    
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
@@ -55,6 +60,8 @@ public class RobotContainer {
         );
 
         joystick.a().whileTrue(vision.print());
+        
+        joystick.b().onTrue(Climby.climbUp().andThen(Climby.climbUp()).andThen(Climby.climbUp()));
 
         // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
--- src/main/java/frc/robot/subsystems/Climby.java
+++ src/main/java/frc/robot/subsystems/Climby.java
@@ -13,68 +13,39 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.RobotContainer;
+
 
 @SuppressWarnings("unused")
 public class Climby extends SubsystemBase {
-    /** Creates a new Climby. */    
-    int[] arrayValues = {300,350,400,650};
+    /** Creates a new Climby. */
+    //We need to set the Enum Value
+    public double targetHeight = 1000;
     public double digitalRotation = 0;
     public double rotationSpeed = 5;
     public double targetEnum = 0;
     public int arrayIndex = 0;
-  
+    public Trigger positionReached = new Trigger(() -> getDigitalRotation() < 0.01);
+
     public Climby(){
   double digitalRotation = 0;
   }
 
-  //This fuction climbs up until it reaches the Enum Value
+  //This fuction climbs up until it reaches the Target Value, and then goes down to zero.
   public Command climbUp(){
     return new RunCommand(()->{
-    if (upKey()) {
-      //Finds the next targetEnum value
-      arrayIndex = 0;
-      while(!(arrayValues[arrayIndex]>getDigitalRotation())||arrayIndex>=arrayValues.length) {
-        arrayIndex += 1;
+      setDigitalRotation(targetHeight);
+      while (getDigitalRotation() >= 0) {
+        setDigitalRotation(getDigitalRotation() - getRotationSpeed());
       };
-      targetEnum = arrayValues[arrayIndex];
-      //Goes up until it reaches the next value
-      while(!((getDigitalRotation()>=targetEnum)||downKey())){
-      rotationSpeed += 0;
-      setDigitalRotation(constrain(getDigitalRotation() + getRotationSpeed(),getDigitalRotation(),targetEnum));
-      }
-    }
-    });
+    }).until(positionReached);
   }
-
-  //This fuction climbs down until it reaches the Enum Value
-  //It is the same as the one above except flipped.
-  public Command climbDown(){
-     return new RunCommand(()->{
-    if (downKey()) {
-      //Finds the next targetEnum value
-      arrayIndex = arrayValues.length;
-      while(!(arrayValues[arrayIndex]<getDigitalRotation())||arrayIndex<=1) {
-        arrayIndex -= 1;
-      };
-      targetEnum = arrayValues[arrayIndex];
-      //Goes down until it reaches the next value
-      while(!((getDigitalRotation()<=targetEnum)||upKey())){
-      rotationSpeed += 0;
-      setDigitalRotation(constrain(getDigitalRotation() - getRotationSpeed(),getDigitalRotation(),targetEnum));
-      }
-    }
-    });
-  }
-
-  public Command Climb(){
-    return new RunCommand(()->{
-    while(true) {
-    climbUp();
-    climbDown();
-    }
-    });
-  }
-
+ 
   //Do it just cuz.
   public double getDigitalRotation(){
     return digitalRotation;
@@ -97,15 +68,6 @@ public class Climby extends SubsystemBase {
     return rotationSpeed;
   }
   
-  //Should this be put into a different subsystem?
-  //I still need to actually bind this to the controller.
-  public Boolean upKey (){
-    return(true);
-  }
-  public Boolean downKey (){
-    return (false);
-  }
-  
   public double constrain (double value, double min, double max){
     if (max < min){
       return (constrain(value, max, min));
@@ -118,9 +80,6 @@ public class Climby extends SubsystemBase {
     }
     return (value);
   }
-
-  //Is this using the function?
-  {Climb();};
 
   //¿Por qué no?
   public Command nothing(){
