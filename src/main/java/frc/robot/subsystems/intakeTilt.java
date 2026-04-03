53b2ca4173d5f4a28bae0be46304ad02b361f197
MajesticGenuis <montanaelisabethcook@gmail.com>
2026-04-02 22:00:56 -0400

    import fix

--- src/main/java/frc/robot/subsystems/intakeTilt.java
+++ src/main/java/frc/robot/subsystems/intakeTilt.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
